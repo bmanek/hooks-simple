fd77fc20d87606edb09ffa831d8da7157345fbcd
bmanek <41875460+bmanek@users.noreply.github.com>
2020-03-29 20:47:04 -0400

    remove boilerplate templating, create simple state mgmt, refactor with hooks

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [resource, setResource] = useState('posts')
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('to dos')}>To Dos</button>
+      </div>
+      {resource}
+    </div>
+  )
+}
+
+export default App
