b0fec7e7de5125d85abc5d9efefddc7b15d03357
bmanek <41875460+bmanek@users.noreply.github.com>
2020-03-29 23:03:09 -0400

    add counter buttons, stub ResourceList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
+import ResourceList from './ResourceList'
 
 const App = () => {
   const [resource, setResource] = useState('posts')
+  let [count, setCount] = useState(0)
 
   return (
     <div>
       <div>
         <button onClick={() => setResource('posts')}>Posts</button>
         <button onClick={() => setResource('to dos')}>To Dos</button>
+        <br/>
+        <button onClick={() => setCount(++count)}>+</button>
+        <button onClick={() => setCount(--count)}>-</button>
       </div>
       {resource}
+      <br/>
+      {count}
+      <ResourceList />
     </div>
   )
 }
---
+++ src/components/ResourceList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class ResourceList extends React.Component {
+  render() {
+    return <div>Hey You GUYSSS</div>
+  }
+}
+
+export default ResourceList
